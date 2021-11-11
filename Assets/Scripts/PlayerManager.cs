8baf080c008274d4c22332a18d50d07f0bb8d11b
aivxx <90653195+aivxx@users.noreply.github.com>
2021-11-11 14:13:49 -0500

    Attempt to set collectable to false

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+
+    public CharacterController player;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // Collect pick ups
+        if (other.CompareTag("Pick Up"))
+        {
+            Debug.Log("Pick up collected!");
+            GameObject.Destroy(other.gameObject);
+        }
+    }
+}
